f550b3f46a3d321afa9fb406eb97ae79a105fb6c
AzizGit404 <its3aziz06@gmail.com>
2026-05-18 10:54:19 +0200

    Delete FirstRestAPI directory

--- FirstRestAPI/task2_project/src/main/java/api/product/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package api.product.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import api.product.domain.Product;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
